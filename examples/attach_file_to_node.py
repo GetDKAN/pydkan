56ccca522e82730cc9163ceec1065fffe360b505
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-06-28 17:39:22 -0300

    updates attach_file example

--- examples/attach_file_to_node.py
+++ examples/attach_file_to_node.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('..')
-
 import os
 import json
 from dkan.client import DatasetAPI
@@ -16,23 +13,6 @@ api = DatasetAPI(
   'admin',
   True
 )
-
-# Get the resource node which file we want to update
-resource = api.node(RESOURCE_ID, 'retrieve')
-resource = resource.json()
-
-# Check if the field for the file is empty.
-if type(resource['field_upload']) != list:
-  # If it's not empty the field
-  data = {
-    'field_upload': {
-      'und': [{}],
-    },
-  }
-  data = json.dumps(data)
-  r = api.node(RESOURCE_ID, 'update', data=data)
-  print r.status_code
-  print r.text
 
 # Attach the file to the resource node
 r = api.attach_file_to_node(CSV, RESOURCE_ID)
